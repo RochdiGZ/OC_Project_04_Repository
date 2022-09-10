ffcdf1615e4d7eda159341922505f26d279478de
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2022-09-11 00:46:45 +0100

    Add the file named players_controller.py

--- controllers/players_controller.py
+++ controllers/players_controller.py
@@ -5,15 +5,17 @@ from views.players_view import PView
 class Controller:
     """Main controller."""
 
-    def __init__(self, player: "Player", p_view: "PView"):
-        self.player = player
-        self.p_view = p_view
+    def __init__(self, p: "Player", pv: "PView"):
+        self.p = p
+        self.pv = pv
 
     def add_players(self) -> list:
-        return self.p_view.save_players(self.player)
+        return self.pv.save_players_in_list()
 
 
 if __name__ == "__main__":
-    p_controller = Controller(player=Player(), p_view=PView())
+    player = Player()
+    p_view = PView()
+    p_controller = Controller(player, p_view)
     list_of_players = p_controller.add_players()
-    PView().output(list_of_players)
+    p_view.output(list_of_players)
