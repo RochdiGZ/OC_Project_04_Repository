1cc57d9c984b73d88aeb178a8bc43e985268b673
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2022-09-11 00:46:00 +0100

    Add the file named players_view.py

--- views/players_view.py
+++ views/players_view.py
@@ -6,20 +6,29 @@ number_of_players = 2
 
 class PView:
     """Define the player view."""
-
-    def __init__(self):
-        self.players = []
+    players = []
 
     @staticmethod
-    def enter_data(p: "Player") -> dict:
+    def enter_data(p: "Player"):
         """Enter the data of player."""
-        p.first_name = input("Enter the first name : ")
-        p.last_name = input("Enter the last name : ")
-        p.date_of_birth = input("Enter the date of birth : ")
-        p.gender = input("Enter the gender : ")
-        p.ranking = int(input("Enter the ranking : "))
-        p.score = int(input("Enter the score : "))
-        return p.serial_player()
+        p.first_name = input("-" * 15 + " Enter the first name : ")
+        p.last_name = input("-" * 15 + " Enter the last name : ")
+        p.date_of_birth = input("-" * 15 + " Enter the date of birth : ")
+        p.gender = input("-" * 15 + " Enter the gender : ")
+        p.ranking = int(input("-" * 15 + " Enter the ranking : "))
+        p.score = int(input("-" * 15 + " Enter the score : "))
+
+    @staticmethod
+    def serial_player(p: "Player") -> dict:
+        """Serialize the class 'Player'."""
+        return {
+            "first_name": p.first_name,
+            "last_name": p.last_name,
+            "date_of_birth": p.date_of_birth,
+            "gender": p.gender,
+            "ranking": p.ranking,
+            "score": p.score
+            }
 
     @staticmethod
     def create_database(file_name: str) -> "TinyDB":
@@ -31,27 +40,33 @@ class PView:
         """Create the table to insert the players' data."""
         return database.table(table_name)
 
-    def save_players(self, p: "Player") -> list:
+    def save_players_in_list(self) -> list:
+        print("-" * 5, " Enter the data players in the tournament ", "-" * 5)
+        for i in range(number_of_players):
+            print("-" * 10, " Enter the data player", i + 1, "-" * 10)
+            p = Player()
+            """Enter the player data."""
+            self.enter_data(p)
+            """Add a player into the list of dicts"""
+            dict_player = self.serial_player(p)
+            self.players.append(dict_player)
+        return self.players
+
+    def save_players_in_db(self):
         """Define an instance of the class Player."""
         players_db = self.create_database("players_db")
         players_table = self.create_table("players", players_db)
-        """Remove the all records of the table"""
+        """Remove the all records from the table"""
         players_table.truncate()
-        for i in range(number_of_players):
-            print("-" * 10, " Enter the data player", i + 1, "-" * 10)
-            """Enter the player data."""
-            dict_player = self.enter_data(p)
-            """Save the player data into the table 'players'."""
-            p.save_player("players", players_db)
-            """Add a player into the list of dicts"""
-            self.players.append(dict_player)
-        return self.players
+        """Save the players data into the table named 'players'."""
+        dict_players = self.save_players_in_list()
+        players_db.table("players").insert_multiple(dict_players)
 
     @staticmethod
     def output(players: list):
         print("-" * 10, " List of players in the tournament ", "-" * 10)
-        for i in range(number_of_players):
-            print("Player", i + 1, ":", players[i])
+        for k in range(len(players)):
+            print("Player", k + 1, ":", players[k])
 
 
 if __name__ == "__main__":
@@ -59,5 +74,5 @@ if __name__ == "__main__":
     player = Player()
     """Define an instance of the class View."""
     p_view = PView()
-    list_of_players = p_view.save_players(player)
+    list_of_players = p_view.save_players_in_list()
     p_view.output(list_of_players)
