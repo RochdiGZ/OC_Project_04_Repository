9b97af5dbb53ca371c84edc4a9724852733f071b
Rochdi GUEZGUEZ <Rochdi@GUEZGUEZ.Gmail.Com>
2022-11-07 17:34:25 +0100

    Add the controllers files

---
+++ controllers/main_controller.py
@@ -0,0 +1,149 @@
+from tinydb import TinyDB
+from datetime import datetime
+from time import sleep
+# import models
+from models.match_model import Match
+from models.player_model import Player
+from models.report_model import Report
+from models.round_model import Round
+from models.tournament_model import Tournament
+# import views
+from views.matches_views import Matches
+from views.menus_view import Menus
+from views.players_view import Players
+from views.reports_view import Reports
+from views.rounds_view import Rounds
+from views.tournaments_view import Tournaments
+DB = TinyDB("chess_db.json", indent=4, sort_keys=False)
+PARTICIPANTS_NUMBER = 8
+
+
+class MainController:
+    """Main controller."""
+    def __init__(self):
+        """Initialize models and views."""
+        self.menus = Menus()
+        self.players = Players()
+        self.tournaments = Tournaments()
+        self.rounds = Rounds()
+        self.matches = Matches()
+        self.reports = Reports()
+
+    def create_player(self) -> "Player":
+        """Enter the player data."""
+        first_name = self.players.enter_first_name()
+        last_name = self.players.enter_last_name()
+        date_of_birth = self.players.enter_date_of_birth()
+        gender = self.players.enter_gender()
+        player = Player(first_name, last_name, date_of_birth, gender)
+        self.players.display_enter_player_ranking(player.index)
+        player.ranking = self.players.enter_ranking()
+        return player
+
+    def add_player(self):
+        new_player = self.create_player()
+        new_player.add_player_in_db()
+        self.players.display_player_created()
+
+    def add_players(self):
+        for _ in range(PARTICIPANTS_NUMBER):
+            new_player = self.create_player()
+            self.players.display_player_created()
+            new_player.add_player_in_db()
+
+    def create_tournament(self, player_indexes: list) -> "Tournament":
+        """Enter the tournament data."""
+        self.tournaments.display_enter_tournament_data()
+        name_t = self.tournaments.enter_name_t()
+        location_t = self.tournaments.enter_location_t()
+        date_t = datetime.now().strftime("%x")
+        time_control = self.tournaments.enter_time_control()
+        description = self.tournaments.enter_description()
+        participants_indexes = self.players.enter_indexes(player_indexes, PARTICIPANTS_NUMBER)
+        rounds_number = self.tournaments.enter_number_of_rounds()
+        return Tournament(date_t, participants_indexes, name_t, location_t, time_control, description, rounds_number)
+
+    @staticmethod
+    def select_tournament(tournament_index: int) -> dict:
+        tournament_data = Tournament.extract_tournament_data(tournament_index)
+        return tournament_data
+
+    @staticmethod
+    def get_participants_tuples(participants_data: list) -> list:
+        participants_tuples = []
+        for i in range(len(participants_data)):
+            participants_tuples.append((participants_data[i]["ranking"], participants_data[i]["score"],
+                                       participants_data[i]))
+        return participants_tuples
+
+    def start_matches_of_round(self, round_index, participants_pairs: list) -> "Round":
+        round_i = Round(round_index)
+        # Add the round matches
+        for index, pair in enumerate(participants_pairs, 1):
+            match_i = Match(index, pair[0], pair[1])
+            match_i.name_m = round_i.round_name + " " + match_i.name_m
+            round_i.matches.append(match_i)
+        # Status display at the start of the round
+        round_i.begin_date_time = datetime.now().strftime("%x %X")
+        self.rounds.display_round_created(round_index)
+        self.rounds.display_start_new_round(round_index)
+        self.rounds.display_round_model(round_i)
+        for match_i in round_i.matches:
+            self.matches.display_match_model(match_i)
+        sleep(2)
+        self.matches.display_enter_matches_results()
+        for match_i in round_i.matches:
+            # Enter the match result and update the players pair sores
+            match_i.result = self.matches.enter_match_result(match_i.name_m)
+            match_i.player1["score"], match_i.player2["score"] = match_i.update_participant_score(match_i.result)
+            # At the end of match, Updating participants scores in the players table by players pairs
+            self.players.display_player_score_updated(match_i.player1["index"])
+            Player.update_player_score(match_i.player1["index"], match_i.player1["score"])
+            self.players.display_player_score_updated(match_i.player2["index"])
+            Player.update_player_score(match_i.player2["index"], match_i.player2["score"])
+        sleep(2)
+        # Status display at the end of the round
+        for match_i in round_i.matches:
+            self.matches.display_match_model(match_i)
+        round_i.end_date_time = datetime.now().strftime("%x %X")
+        self.rounds.display_round_model(round_i)
+        self.rounds.display_end_round(round_index)
+        return round_i
+
+    def start_tournament(self, tournament_index: int) -> dict:
+        self.tournaments.display_start_new_tournament(tournament_index)
+        tournament_data = self.select_tournament(tournament_index)
+        participants_indexes = tournament_data["participants"]
+        participants_data = Player.extract_participants_data(participants_indexes)
+        # Set participants scores to 0
+        # for participant in participants_data: participant["score"] = 0.0
+        for i in range(1, tournament_data["rounds_number"]+1):
+            if i == 1:
+                participants_data = Report.sorted_list_by(participants_data, "ranking")
+            elif i > 1:
+                participants_data = Report.sorted_list_by(participants_data, "score")
+                participants_data = Report.sort_duplicated_data(participants_data)
+            participants_data_pairs = Player.get_participants_pairs(participants_data)
+            # Get the round data
+            round_i = self.start_matches_of_round(i, participants_data_pairs)
+            # At the end of round, Updating of the tournament rounds in the tournaments table
+            matches_list = []
+            for match in round_i.matches:
+                matches_list.append(match.serialize_match())
+            round_i.matches = matches_list
+            round_data = round_i.serialize_round()
+            tournament_data["rounds"].append(round_data)
+            Tournament.update_tournament(tournament_index, tournament_data["rounds"])
+
+        return tournament_data
+
+    def updating_after_end_tournament(self, tournament_data: dict):
+        response = self.tournaments.enter_response()
+        if response.lower() in ["yes", "y"]:
+            participants_indexes = tournament_data["participants"]
+            for index in participants_indexes:
+                self.players.display_enter_player_ranking(index)
+                new_ranking = self.players.enter_ranking()
+                Player.update_player_ranking(index, new_ranking)
+        else:
+            self.tournaments.display_no_updating_rankings()
---
+++ controllers/menu_controller.py
@@ -0,0 +1,162 @@
+from tinydb import TinyDB
+# import models
+from models.chess_db_model import Participant
+from models.player_model import Player
+from models.tournament_model import Tournament
+from models.report_model import Report
+# import views
+from views.menus_view import Menus
+from views.players_view import Players
+from views.tournaments_view import Tournaments
+from views.reports_view import Reports
+from controllers.main_controller import MainController
+
+DB = TinyDB("chess_db.json", indent=4, sort_keys=False)
+PARTICIPANTS_NUMBER = 8
+
+
+class MenuController:
+    """Main controller."""
+    def __init__(self):
+        """Initialize models and views."""
+        self.menus = Menus()
+        self.players = Players()
+        self.tournaments = Tournaments()
+        self.reports = Reports()
+        self.controllers = MainController()
+
+    def main_menu(self):
+        self.menus.display_main_menu()
+        number = self.menus.choice(4)
+        match number:
+            case 1:
+                if len(DB.table("players")) == 0:
+                    # Generate and add some participants in the database named chess_db
+                    self.reports.display_table_is_empty("players")
+                    self.players.display_generating_database(PARTICIPANTS_NUMBER)
+                    Participant.generate_add_participants(PARTICIPANTS_NUMBER)
+                    self.main_menu()
+                else:
+                    self.access_to_players_menu()
+            case 2:
+                self.access_to_tournaments_menu()
+            case 3:
+                self.access_to_reports_menu()
+            case 4:
+                exit()
+
+    def access_to_players_menu(self):
+        self.menus.display_players_menu()
+        choice = self.menus.choice(3)
+        if choice == 1:
+            # Add a player data in database
+            self.controllers.add_player()
+            self.access_to_players_menu()
+        elif choice == 2:
+            if len(DB.table("players")) == 0:
+                self.reports.display_table_is_empty("players")
+            else:
+                # update a player ranking in database
+                player_index = self.players.enter_player_index()
+                new_ranking = self.players.enter_ranking()
+                Player.update_player_ranking(player_index, new_ranking)
+            self.access_to_players_menu()
+        elif choice == 3:
+            # Back to main menus.
+            self.main_menu()
+
+    def create_add_tournament_in_db(self):
+        if len(DB.table("players")) == 0:
+            self.reports.display_table_is_empty("players")
+        elif len(DB.table("players")) < PARTICIPANTS_NUMBER:
+            self.reports.display_not_enough_players()
+        else:
+            # Add tournament's data in database
+            players_indexes = Player.extract_players_data("index")
+            new_tournament = self.controllers.create_tournament(players_indexes)
+            Tournament.add_tournament_in_db(new_tournament)
+            tournament_data = Tournament.extract_tournament_data(new_tournament.index)
+            if len(tournament_data["rounds"]) == 0:
+                self.tournaments.display_tournament_created(tournament_data["index"])
+                tournament_data = new_tournament.serialize_tournament()
+                tournament_data = self.controllers.start_tournament(tournament_data["index"])
+                self.tournaments.display_end_tournament(tournament_data["index"])
+            # At the end of tournament, Updating the participants rankings
+            self.controllers.updating_after_end_tournament(tournament_data)
+
+    def select_update_tournament_in_db(self):
+        if len(DB.table("players")) == 0:
+            self.reports.display_table_is_empty("players")
+        elif len(DB.table("players")) < PARTICIPANTS_NUMBER:
+            self.reports.display_not_enough_players()
+        elif len(DB.table("tournaments")) == 0:
+            self.reports.display_table_is_empty("tournaments")
+        else:
+            # Select and start a tournament
+            tournaments_indexes = Tournament.extract_tournaments_indexes()
+            tournament_index = self.tournaments.enter_tournament_index(tournaments_indexes)
+            tournament_data = Tournament.extract_tournament_data(tournament_index)
+            if len(tournament_data["rounds"]) == 0:
+                tournament_data = self.controllers.start_tournament(tournament_index)
+                self.tournaments.display_end_tournament(tournament_index)
+            # At the end of tournament, Updating the participants rankings
+            self.controllers.updating_after_end_tournament(tournament_data)
+
+    def access_to_tournaments_menu(self):
+        self.menus.display_tournaments_menu()
+        choice = self.menus.choice(3)
+        if choice == 1:
+            self.create_add_tournament_in_db()
+        elif choice == 2:
+            self.select_update_tournament_in_db()
+        elif choice == 3:
+            # Back to main menu.
+            self.main_menu()
+        # Back to tournaments menu.
+        self.access_to_tournaments_menu()
+
+    def access_to_reports_menu(self):
+        self.menus.display_reports_menu()
+        choice = self.menus.choice(6)
+        if choice == 1 or choice == 2:
+            if len(DB.table("players")) == 0:
+                self.reports.display_table_is_empty("players")
+            elif choice == 1:
+                # Sort all players by first name
+                players_list = Report.sorted_table_by(DB.table("players"), "first_name")
+                # Display the sorted all players by name
+                self.reports.display_players(players_list, "first_name")
+            else:
+                # Sort all players by first name
+                players_list = Report.sorted_table_by(DB.table("players"), "ranking")
+                # Display the sorted all players by ranking
+                self.reports.display_players(players_list, "ranking")
+        elif choice == 3:
+            # Display the all tournaments
+            if len(DB.table("tournaments")) == 0:
+                self.reports.display_table_is_empty("tournaments")
+            else:
+                tournaments_list = Report.sorted_table_by(DB.table("tournaments"), "index")
+                self.reports.display_tournaments(tournaments_list)
+        elif choice == 4 or choice == 5:
+            if len(DB.table("tournaments")) == 0:
+                self.reports.display_table_is_empty("tournaments")
+            else:
+                tournaments_indexes = Tournament.extract_tournaments_indexes()
+                tournament_index = self.tournaments.enter_tournament_index(tournaments_indexes)
+                # Select a tournament
+                tournament_data = self.controllers.select_tournament(tournament_index)
+                participants_indexes = tournament_data["participants"]
+                participants_data = Player.extract_participants_data(participants_indexes)
+                if choice == 4:
+                    # Sort players by name
+                    participants_data = Report.sorted_list_by(participants_data, "first_name")
+                    self.reports.display_players(participants_data, "first_name")
+                else:
+                    # Sort players by ranking
+                    participants_data = Report.sorted_list_by(participants_data, "ranking")
+                    self.reports.display_players(participants_data, "ranking")
+                self.reports.display_rounds_matches(tournament_data["rounds"])
+        elif choice == 6:
+            self.main_menu()
+        self.access_to_reports_menu()
